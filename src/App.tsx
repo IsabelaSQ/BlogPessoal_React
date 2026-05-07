3e8d953e9f56f16b107729bfd1e1e820de21e1e2
Isabela <isabela_siqueirasilva@hotmail.com>
2026-05-07 10:53:15 -0300

    feat: Implementação das páginas de cadastro e login, além da criação dos modelos de usuário,

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,28 @@
-import './App.css'
-import Footer from './components/footer/footer'
-import Navbar from './components/navbar/navbar'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/home/home'
+import Navbar from './components/navbar/navbar'
+import Footer from './components/footer/footer'
+import Cadastro from './pages/cadastro/cadastro'
+import Login from './pages/login/login'
+
 
 function App() {
-  return (
-    <>
-    <Navbar />
-    <Home />
-    <Footer />
-    </>
-  )
+	return (
+		<>
+			<BrowserRouter>
+				<Navbar />
+				<div className="min-h-[80vh]">
+					<Routes> 
+            <Route path="/" element={<Login />} />
+						<Route path="/home"	element={<Home />} />
+            <Route path="/cadastro" element={<Cadastro />} />
+            <Route path="/login" element={<Login />} />
+					</Routes>
+				</div>
+				<Footer />
+			</BrowserRouter>
+		</>
+	)
 }
 
 export default App
