e13bd56326bc8c8f5ed78248c96c2a662a577f2b
Isabela <isabela_siqueirasilva@hotmail.com>
2026-05-14 11:59:25 -0300

    feat

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Footer from './components/Footer/Footer'
-import Navbar from './components/Navbar/Navbar'
 import FormPostagem from './components/postagem/formpostagem/FormPostagem'
 import ListaPostagens from './components/postagem/listapostagens/ListaPostagens'
 import DeletarTema from './components/tema/deletartema/DeletarTema'
 import FormTema from './components/tema/formtema/FormTema'
 import ListaTemas from './components/tema/listatemas/ListaTemas'
+import Perfil from './pages/perfil/Perfil'
+import { AuthProvider } from './context/AuthContext'
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify'
+import DeletarPostagem from './components/postagem/deletarpostagem/DeletarPostagen'
+import Footer from './components/Footer/Footer'
+import Navbar from './components/Navbar/Navbar'
 import Cadastro from './pages/Cadastro/Cadastro'
 import Home from './pages/Home/Home'
 import Login from './pages/Login/Login'
-import Perfil from './pages/perfil/Perfil'
-import { AuthProvider } from './context/AuthContext'
-import DeletarPostagem from './components/postagem/deletarpostagem/DeletarPostagen'
-import 'react-toastify/dist/ReactToastify.css';
-import { ToastContainer } from 'react-toastify'
 
 function App() {
 	return (
